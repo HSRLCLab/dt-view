a1b7890dc6ab809f7c62d283224e5eff28675714
Sandro Scheiwiller <sandro@012345.ch>
2018-02-05 18:30:07 +0100

    enable rendering of model

--- src/components/Visualisation.vue
+++ src/components/Visualisation.vue
@@ -1,16 +1,19 @@
 <template lang="pug">
 div.visualisation(
   ref="three"
-  @click="detailSelected"
 )
 </template>
 
 <script>
 /*eslint no-unused-vars: "off", no-undef: "off"*/
 import * as THREE from 'three';
+import model1 from 'file-loader!../assets/32184-4211621.wrl';
+import model2 from 'file-loader!../assets/CAD-LA000053.wrl';
 
 window.THREE = THREE;
 require('three/examples/js/controls/OrbitControls');
+require('three/examples/js/loaders/VRMLLoader');
+var renderer, scene, camera;
 
 export default {
   name: 'Visualisation',
@@ -19,17 +22,42 @@ export default {
     console.log({ ref });
     const { clientHeight: height, clientWidth: width } = ref;
 
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    scene.add(camera);
+    camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    camera.position.z = 10;
+    window.camera = camera;
+
     const controls = new THREE.OrbitControls(camera);
-    const renderer = new THREE.WebGLRenderer();
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff);
+    scene.add(camera);
+
+    // light
+    const dirLight = new THREE.DirectionalLight(0xffffff);
+    dirLight.position.set(2000, 2000, 2000).normalize();
+    camera.add(dirLight);
+    camera.add(dirLight.target);
+
+    const loader = new THREE.VRMLLoader();
+    loader.load(model2, function(object) {
+      scene.add(object);
+    });
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(width, height);
+
     ref.appendChild(renderer.domElement);
+
+    this.animate();
   },
   methods: {
     detailSelected(event) {
       this.$emit('detailSelected', event);
+    },
+    animate() {
+      requestAnimationFrame(this.animate);
+      renderer.render(scene, camera);
     }
   }
 };
